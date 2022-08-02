7a7e1823baf8bb012b6c4eb5d0b9e364c189779c
Gouse Basha <mohammadgous526@gmail.com>
2022-08-02 17:07:13 +0530

    Building Landing page

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import { Container } from "semantic-ui-react";
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,13 @@
+import EventListItem from "./EventListItem";
+
+const EventList = () => {
+  return (
+    <div>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </div>
+  );
+};
+export default EventList;
