cdfa982864ffddc502457b136590ce710b3b90ff
Gouse Basha <mohammadgous526@gmail.com>
2022-08-03 10:31:12 +0530

    Creating Events

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -3,17 +3,27 @@ import EventForm from "../eventForm/EventForm";
 import EventList from "./EventList";
 import { sampleData } from "../../../app/api/sampleData";
 import { useState } from "react";
-const EventDashboard = ({formOpen, setFormOpen}) => {
+const EventDashboard = ({ formOpen, setFormOpen }) => {
   const [events, setEvents] = useState(sampleData);
- 
+
+  function handleCreateEvents(event) {
+    setEvents([...events, event]);
+  }
+
   return (
     <Grid>
       <Grid.Column width={10}>
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen}/>}
-        </Grid.Column>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvents={handleCreateEvents}
+          />
+        )}
+      </Grid.Column>
     </Grid>
   );
 };
