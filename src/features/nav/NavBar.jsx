3e5b4cf8e66376851c80da7dfdc654eba9b198ce
Gouse Basha <mohammadgous526@gmail.com>
2022-08-10 16:42:53 +0530

    implemented-fake-auth

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,25 +1,43 @@
-import { NavLink } from 'react-router-dom';
-import {Menu, Container,Button} from 'semantic-ui-react'
-const NavBar = ({setFormOpen}) => {
+import { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { Menu, Container, Button } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
+import SignedOutMenu from "./SignedOutMenu";
+const NavBar = ({ setFormOpen }) => {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
 
-    return(
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item as={NavLink} exact to='/' header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: 15}} />
-                    Plevent
-                </Menu.Item>
-                <Menu.Item as={NavLink} to='/events' name="Events" />
-                <Menu.Item as={NavLink} to='/createEvent'>
-                    <Button onClick={()=>setFormOpen(true)} positive inverted content = 'Create Event' />
-                </Menu.Item>
-                <Menu.Item position='right'>
-                    <Button basic inverted content='Login'/>
-                    <Button basic inverted content='Register' style={{marginLeft: '0.5em'}}/>
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
-}
+  function handleSignOut() {
+    setAuthenticated(false);
+    history.push("/");
+  }
+
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} exact to="/" header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: 15 }} />
+          Plevent
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events" />
+        {authenticated && (
+          <Menu.Item as={NavLink} to="/createEvent">
+            <Button
+              onClick={() => setFormOpen(true)}
+              positive
+              inverted
+              content="Create Event"
+            />
+          </Menu.Item>
+        )}
+        {authenticated ? (
+          <SignedInMenu signOut={handleSignOut} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
+      </Container>
+    </Menu>
+  );
+};
 
 export default NavBar;
