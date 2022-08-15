d80255a7a2859ffc2fd3de3a360c4a95b89415d7
Gouse Basha <mohammadgous526@gmail.com>
2022-08-15 11:52:53 +0530

    Implementing-Redux

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -5,11 +5,9 @@ import { Container } from "semantic-ui-react";
 import HomePage from "../../features/home/HomePage";
 import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
 import EventForm from "../../features/events/eventForm/EventForm";
+import Sandbox from "../../features/sandbox/Sandbox";
 
 function App() {
-
-
-  
   return (
     <>
       <Route exact path="/" component={HomePage} />
@@ -19,7 +17,7 @@ function App() {
           <>
             <NavBar />
             <Container className="main">
-              <Route exact path="/events" component={EventDashboard} />
+              <Route exact path="/sandbox" component={Sandbox} />
               <Route path="/events/:id" component={EventDetailedPage} />
               <Route path={["/createEvent", '/manage/:id']} component={EventForm} />
             </Container>
